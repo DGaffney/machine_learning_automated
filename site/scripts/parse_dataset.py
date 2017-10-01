8db9b18bd9a4211bdbf9d1f61da39a4939851ce9
Devin Gaffney <itsme@devingaffney.com>
2017-10-01 19:44:12 -0400

    updates

--- site/scripts/parse_dataset.py
+++ site/scripts/parse_dataset.py
@@ -133,9 +133,7 @@ def convert_text_fields_to_data(casted_dataset, manifest):
     cleared_dataset = []
     clean_dataset = np.array(detexted).transpose().tolist()
     for i,label in enumerate(labels):
-        print clean_dataset[i]
-        print [label]
-        if True not in np.isnan(clean_dataset[i]).tolist() and True not in np.isnan([label]).tolist() and None not in clean_dataset[i] and None != label:
+        if None not in clean_dataset[i] and None != label and True not in np.isnan(clean_dataset[i]).tolist() and True not in np.isnan([label]).tolist():
             cleared_labels.append(label)
             cleared_dataset.append(clean_dataset[i])
     return cleared_dataset, cleared_labels, conversion_pipeline
