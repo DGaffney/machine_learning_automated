417bc728dd4d608b19ab1233c56b087fa499b5f6
Devin Gaffney <itsme@devingaffney.com>
2017-10-12 12:34:36 -0400

    updates

--- site/scripts/predictor_fast.py
+++ site/scripts/predictor_fast.py
@@ -55,6 +55,7 @@ def try_model(model, current_best_model):
     if current_best_model[-1] < np.mean(scores):
         current_best_model = [model, np.mean(scores)]
         diagnostics.store_model(current_best_model, x, y, dataset_id, label_type, dataset_filename, storage_location, manifest_filename, conversion_pipeline, diagnostic_image_path)
+    return current_best_model
 
 @timeout(120)
 def try_ensemble_model(models, current_best_model):
@@ -62,17 +63,21 @@ def try_ensemble_model(models, current_best_model):
         model = VotingClassifier([(str(el), el) for el in models], voting="soft")
         scores = cross_val_score(model, x, y, cv=10, scoring=score_type)
     except:
-        model = VotingClassifier([(str(el), el) for el in models])
-        scores = cross_val_score(model, x, y, cv=10, scoring=score_type)
+        try:
+            model = VotingClassifier([(str(el), el) for el in models])
+            scores = cross_val_score(model, x, y, cv=10, scoring=score_type)
+        except:
+            return current_best_model
     if current_best_model[-1] < np.mean(scores):
         current_best_model = [model, np.mean(scores)]
         diagnostics.store_model(current_best_model, x, y, dataset_id, label_type, dataset_filename, storage_location, manifest_filename, conversion_pipeline, diagnostic_image_path)
+    return current_best_model
 
 i = 1
 current_best_model = [None, -10000000.0]
 best_performing_models = []
 for model in models:
-    try_model(model, current_best_model)
+    current_best_model = try_model(model, current_best_model)
     i += 1
 
 if current_best_model == [None, -10000000.0]:
@@ -83,7 +88,7 @@ if current_best_model == [None, -10000000.0]:
     i = 1
     current_best_model = [None, -10000000.0]
     for model in models:
-        try_model(model, current_best_model)
+        current_best_model = try_model(model, current_best_model)
         i += 1
 
 if len(best_performing_models) > 1:
@@ -91,6 +96,6 @@ if len(best_performing_models) > 1:
         model_count += 2
         for i in range(run_count):
             models = list(diagnostics.random_combination(best_performing_models, model_count))
-            try_ensemble_model(models, current_best_model)
+            current_best_model = try_ensemble_model(models, current_best_model)
 
 diagnostics.store_model(current_best_model, x, y, dataset_id, label_type, dataset_filename, storage_location, manifest_filename, conversion_pipeline, diagnostic_image_path)
