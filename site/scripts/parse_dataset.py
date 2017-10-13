21680dec05c9a20e47c53dcc1950c7e4957c9a86
Devin Gaffney <itsme@devingaffney.com>
2017-10-13 14:13:41 -0400

    updates

--- site/scripts/parse_dataset.py
+++ site/scripts/parse_dataset.py
@@ -111,6 +111,7 @@ def convert_text_fields_to_data(casted_dataset, manifest):
     labels = []
     conversion_pipeline = {}
     for i,col in enumerate(transposed):
+        print i
         if i == int(manifest['prediction_column']):
             if type(col[0]) == type([]) or type(col[0]) == type(()):
                 labels = [str.join(" ", el) for el in col]
@@ -122,16 +123,16 @@ def convert_text_fields_to_data(casted_dataset, manifest):
             stop = set(stopwords.words('english'))
             exclude = set(string.punctuation)
             lemma = WordNetLemmatizer()
-            # try:
-            tfidf_matrix =  tf.fit_transform([str.join(" ", el) for el in col])
-            feature_names = tf.get_feature_names()
-            word_scores = Counter()
-            for doc_i in range(tfidf_matrix.shape[0]):
-                for word_index in tfidf_matrix[doc_i,:].nonzero()[1]:
-                    word_scores[feature_names[word_index]] += tfidf_matrix[doc_i, word_index]
-            unique_terms = [el[0] for el in word_scores.most_common(200)]
-            # except:
-            #     unique_terms = [el[0] for el in Counter(list([item for sublist in col for item in sublist])).most_common(200)]
+            try:
+                tfidf_matrix =  tf.fit_transform([str.join(" ", el) for el in col])
+                feature_names = tf.get_feature_names()
+                word_scores = Counter()
+                for doc_i in range(tfidf_matrix.shape[0]):
+                    for word_index in tfidf_matrix[doc_i,:].nonzero()[1]:
+                        word_scores[feature_names[word_index]] += tfidf_matrix[doc_i, word_index]
+                unique_terms = [el[0] for el in word_scores.most_common(200)]
+            except ValueError:
+                unique_terms = [el[0] for el in Counter(list([item for sublist in col for item in sublist])).most_common(200)]
             conversion_pipeline[i] = {"unique_terms": unique_terms}
             counteds = []
             for term in unique_terms:
