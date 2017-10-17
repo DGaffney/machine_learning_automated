0ffdbc3faeb1b978a7d192e55a5836b8fe4316d0
Devin Gaffney <itsme@devingaffney.com>
2017-10-17 18:56:49 -0400

    updates

--- site/scripts/diagnostics.py
+++ site/scripts/diagnostics.py
@@ -191,7 +191,7 @@ def random_combination(iterable, r):
     "Random selection from itertools.combinations(iterable, r)"
     pool = tuple(iterable)
     n = len(pool)
-    indices = sorted(random.sample(xrange(n), r))
+    indices = sorted(random.sample(range(n), r))
     return tuple(pool[i] for i in indices)
 
 
