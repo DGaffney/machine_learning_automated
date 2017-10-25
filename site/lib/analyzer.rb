55c23580530628f5503e9e01f8378502dfbdbe7b
Devin Gaffney <itsme@devingaffney.com>
2017-10-25 13:03:01 -0400

    updates

--- site/lib/analyzer.rb
+++ site/lib/analyzer.rb
@@ -16,8 +16,8 @@ module Analyzer
       script = @dataset.prediction_speed == 2 ? "predictor_fast" : "predictor_main"
       current_statement = {}
       statements = []
-      puts "python3.5 scripts/predictor_fast.py #{filename} #{filename.gsub(".csv", "")+"_manifest.json"} #{@dataset.col_classes[@dataset.prediction_column]} #{@dataset.prediction_speed}"
-      IO.popen("python3.5 scripts/predictor_fast.py #{filename} #{filename.gsub(".csv", "")+"_manifest.json"} #{@dataset.col_classes[@dataset.prediction_column]} #{@dataset.prediction_speed}") do |io|
+      puts "python3.5 scripts/predictor_fast.py #{filename} #{filename.gsub(".csv", "")+"_manifest.json"} #{@dataset.col_classes[@dataset.prediction_column]} #{@dataset.prediction_speed} #{(@dataset.results["best_model"][1] rescue -100000000)}"
+      IO.popen("python3.5 scripts/predictor_fast.py #{filename} #{filename.gsub(".csv", "")+"_manifest.json"} #{@dataset.col_classes[@dataset.prediction_column]} #{@dataset.prediction_speed} #{(@dataset.results["best_model"][1] rescue -100000000)}") do |io|
         io.each_line do |line|
           puts line
           current_statement = JSON.parse(line.strip) rescue nil
