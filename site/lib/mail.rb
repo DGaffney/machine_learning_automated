c58e25302526cc992faca98d371e8e82aa8493a0
Devin Gaffney <itsme@devingaffney.com>
2017-11-02 09:57:50 -0400

    updates

--- site/lib/mail.rb
+++ site/lib/mail.rb
@@ -4,12 +4,13 @@ class Mailer
   end
 
   def self.send(to, subject, html_body, from=SETTINGS["email_from_address"])
-  client.deliver(
-    from: from,
-    to: to,
-    subject: subject,
-    html_body: html_body,
-    track_opens: true
-  )
+    return nil if to == SETTINGS["email_from_address"]
+    client.deliver(
+      from: from,
+      to: to,
+      subject: subject,
+      html_body: html_body,
+      track_opens: true
+    )
   end
 end
